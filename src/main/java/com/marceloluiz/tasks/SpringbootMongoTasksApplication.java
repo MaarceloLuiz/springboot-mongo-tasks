2a193cf48ef35b888a54568a7c3579e6dfb7d783
MaarceloLuiz <marceloluizgpereira@gmail.com>
2024-11-05 10:55:00 +0000

    First Commit - fixed

---
+++ src/main/java/com/marceloluiz/tasks/SpringbootMongoTasksApplication.java
@@ -0,0 +1,13 @@
+package com.marceloluiz.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marceloluiz/tasks/SpringbootMongoTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloluiz.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
